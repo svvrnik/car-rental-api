9f5641a6112c25141069b9d01c2fd26ea21354f8
svvrnik <151081947+svvrnik@users.noreply.github.com>
2026-08-19 15:38:16 +0200

    fix: solve N+1 query problem with EntityGraph

--- core-api/src/main/java/com/example/car_rental_api/car/CarRepository.java
+++ core-api/src/main/java/com/example/car_rental_api/car/CarRepository.java
@@ -1,6 +1,7 @@
 package com.example.car_rental_api.car;
 
 import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,8 +14,10 @@ import java.util.Optional;
 public interface CarRepository extends JpaRepository<Car, Long> {
     boolean existsByLicensePlate(String licensePlate);
 
+    @EntityGraph(attributePaths = {"images"})
     Page<Car> findByStatus(CarStatus status, Pageable pageable);
 
+    @EntityGraph(attributePaths = {"images"})
     Page<Car> findByOwnerEmail(String email, Pageable pageable);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
--- core-api/src/main/java/com/example/car_rental_api/rental/RentalRepository.java
+++ core-api/src/main/java/com/example/car_rental_api/rental/RentalRepository.java
@@ -1,5 +1,6 @@
 package com.example.car_rental_api.rental;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,5 +12,7 @@ import java.time.LocalDateTime;
 public interface RentalRepository extends JpaRepository<Rental, Long> {
     @Query("SELECT CASE WHEN COUNT(r)>0 THEN true ELSE false END FROM Rental r WHERE r.car.id = :carId AND r.status = 'ACTIVE' AND r.startDate <= :endDate AND r.endDate >= :startDate")
     boolean existsOverLappingRental(@Param("carId") Long carId, @Param("startDate")LocalDateTime startDate, @Param("endDate")LocalDateTime endDate);
+
+    @EntityGraph(attributePaths = {"car", "car.images"})
     Page<Rental> findByUserEmail(String email, Pageable pageable);
 }
