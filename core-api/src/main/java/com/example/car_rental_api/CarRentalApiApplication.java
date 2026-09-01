b3839270ce623d4fc48a99bb9bb2fc0e08670bdc
svvrnik <151081947+svvrnik@users.noreply.github.com>
2026-09-01 14:41:03 +0200

    feat: enable scheduling

--- core-api/src/main/java/com/example/car_rental_api/CarRentalApiApplication.java
+++ core-api/src/main/java/com/example/car_rental_api/CarRentalApiApplication.java
@@ -2,8 +2,10 @@ package com.example.car_rental_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CarRentalApiApplication {
 
 	public static void main(String[] args) {
